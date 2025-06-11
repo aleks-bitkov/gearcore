faf0716b6430a15259e3b53761177e965130ba3a
aleks-bitkov <aleks.bitkov.9@gmail.com>
2025-06-11 14:55:38 +0300

    change version Django and python + add templates + add app 'main'

--- config/settings/base.py
+++ config/settings/base.py
@@ -67,7 +67,7 @@ DJANGO_APPS = [
     "django.contrib.sites",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "django.contrib.humanize", # Handy template tags
+    "django.contrib.humanize", # Handy template tags
     "django.contrib.admin",
     "django.forms",
 ]
@@ -88,6 +88,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "gearcore.users",
     # Your stuff: custom apps go here
+    "gearcore.main"
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
